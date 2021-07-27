59413cfa7d963228a8e8c7b2b3fedd2e00968e13
step0035 <step0035@e.ntu.edu.sg>
2021-07-28 00:00:44 +0800

    Add new lora project, rename test_project

--- test_project/main/main.c
+++ test_project/main/main.c
@@ -2,15 +2,21 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-#include "lora.h"
+void hulu_wurld(void *pvParameter) {
+	while(1) {
+		printf("Hulu Wurld\n");
+		vTaskDelay(1000 / portTICK_RATE_MS);
+	}
+}
 
 void HULU(void *pvParameter) {
 	while(1) {
-		printf("%d\n", Add(2, 10));
+		printf("HULU, once every 10 seconds\n");
 		vTaskDelay(10000 / portTICK_RATE_MS);
 	}
 }
 
 void app_main(void) {
+	xTaskCreate(&hulu_wurld, "hulu wurld", 2048, NULL, 5, NULL);
 	xTaskCreate(&HULU, "HULU", 2048, NULL, 1, NULL);
 }
