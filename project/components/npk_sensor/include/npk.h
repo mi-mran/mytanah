88acbf1c8012771a8a3e663cb9e4a00d4812611d
step0035 <step0035@e.ntu.edu.sg>
2021-09-11 01:57:31 +0800

    Add npk sensor register addresses

--- project/components/npk_sensor/include/npk.h
+++ project/components/npk_sensor/include/npk.h
@@ -1,6 +1,8 @@
 #ifndef NPK_H_
 #define NPK_H_
 
+#include <stdint.h>
+
 // UART port and pin numbers
 #define NPK_UART_PORT_NUM          (CONFIG_NPK_UART_PORT_NUM)
 #define NPK_UART_TXD               (CONFIG_NPK_UART_TXD)
@@ -15,6 +17,28 @@
 
 // Timeout threshold for UART = number of symbols (~10 tics) with unchanged state on receive pin
 #define NPK_READ_TOUT             (3) // 3.5T * 8 = 28 ticks, TOUT=3 -> ~24..33 ticks
+
+// NPK address code and function code
+#define NPK_ADDR_CODE           0x01
+#define NPK_FUNC_CODE           0x03
+
+// NPK register addresses
+#define NPK_ADDR_MOISTURE       0x00
+#define NPK_ADDR_TEMP           0x01
+#define NPK_ADDR_COND           0x02
+#define NPK_ADDR_PH             0x03
+#define NPK_ADDR_NITRO          0x04
+#define NPK_ADDR_PHOS           0x05
+#define NPK_ADDR_POTA           0x06
+#define NPK_ADDR_SALIN          0x07
+#define NPK_ADDR_TDS            0x08
+
+// CRC
+#define CRC_LOW_BYTE            0x04
+#define CRC_HIGH_BYTE           0x08
+
+// Modbus command to get moisture ... potassium
+const uint8_t modbus_cmd[] = {NPK_ADDR_CODE,NPK_FUNC_CODE, 0x00, 0x00, 0x00, 0x07, CRC_LOW_BYTE, CRC_HIGH_BYTE};
 
 // Function declarations
 void npk_uart_init(void);
